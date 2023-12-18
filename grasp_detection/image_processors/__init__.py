65e2b8ec494d4109043ebd7f49a2406dc1a7672e
yas777 <yko207@nyu.edu>
2023-12-18 07:26:30 -0500

    Folder Structure Change

--- grasp_detection/image_processors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .image_processor import *
-from .lang_sam_processor import *
-from .sam_processor import *
-from .owl_vit_processor import *
---
+++ src/grasper.sh
@@ -0,0 +1,2 @@
+python grasp.py --checkpoint_path checkpoints/checkpoint_detection.tar --open_communication --debug
+#python top_down_demo.py --checkpoint_path log/checkpoint_detection.tar --top_down_grasp --debug --open_communication
