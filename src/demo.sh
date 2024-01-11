6bfc59c1e17245b7715ad21e1440a9f10606f44e
Chris Paxton <cpaxton@fb.com>
2024-01-11 12:14:45 -0500

    update scripts and add examples for low gpu utilization

--- src/demo.sh
+++ src/demo.sh
@@ -1,3 +1,3 @@
 # python demo.py --checkpoint_path ../checkpoints/checkpoint_detection.tar --open_communication --debug
-python demo.py --checkpoint_path ../checkpoints/checkpoint_detection.tar 
+python demo.py --checkpoint_path ../checkpoints/checkpoint_detection.tar  --max_depth 1.5 --min_depth 0.5 --sampling_rate 0.2
 #python top_down_demo.py --checkpoint_path log/checkpoint_detection.tar --top_down_grasp --debug --open_communication
